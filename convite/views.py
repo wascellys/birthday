15fe003e38266fdabe23a362981fdeeecb50e94c
edy.ferreira <edy.ferreira@bycoders.com.br>
2021-12-03 16:32:11 -0300

    adicionado quantitativo de presentes e ausentes na listagem de convidados

--- convite/views.py
+++ convite/views.py
@@ -9,29 +9,31 @@ from convite.models import Convite, Evento
 def convite(request, codigo):
     convidado = Convite.objects.get(codigo=codigo)
 
-    return render(request, 'convite/convite.html',{'convidado': convidado} )
+    return render(request, 'convite/convite.html', {'convidado': convidado})
 
 
 def confirmar_presenca(request, codigo):
     convidado = Convite.objects.get(codigo=codigo)
     convidado.presente = True
-    convidado.save()
+    # convidado.save()
 
-
-
+    convidado.confirmou_presenca()
     return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
 
 def convidados(request, codigo):
-
     convidados = Convite.objects.filter(evento__codigo=codigo).order_by('pessoa')
-    return render(request, 'convite/convidados.html',{'convidados': convidados})
+    presentes = Convite.objects.filter(evento__codigo=codigo, presente=True).count()
+    ausentes = Convite.objects.filter(evento__codigo=codigo, presente=False).count()
+    return render(request, 'convite/convidados.html',
+                  {'convidados': convidados, 'presentes': presentes, 'ausentes': ausentes})
 
 
 def eventos(request):
     eventos = Evento.objects.all().order_by('nome')
     return render(request, 'convite/eventos.html', {'eventos': eventos})
 
-def gerar_qrcode(request,codigo):
+
+def gerar_qrcode(request, codigo):
     convidados = Convite.objects.filter(evento__codigo=codigo)
     return render(request, 'qrcode.html', {'pessoas': convidados})
